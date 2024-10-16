6861024f72d7aad7541ed7d6ccb6573b79a1ed67
santokan <houska.peter@gmail.com>
2024-10-16 21:56:55 +0200

    added print report

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
     words_count = get_words_count(text)
-    print(f"Book has {words_count} words.")
     chars_count = get_char_count(text)
-    print(chars_count)
+    sorted_list = convert_dict_to_sorted_list(chars_count)
+    print_report(sorted_list, book_path, words_count)
 
 
 def get_text(path):
@@ -27,6 +27,29 @@ def get_char_count(text):
         else:
             chars[char] = 1
     return chars
+
+
+def sort_on(dict):
+    return dict["count"]
+
+
+def convert_dict_to_sorted_list(chars_count):
+    sorted_list = []
+    for ch in chars_count:
+        sorted_list.append({"char": ch, "count": chars_count[ch]})
+    sorted_list.sort(key=sort_on, reverse=True)
+    return sorted_list
+
+
+def print_report(sorted_list, book_path, num_words):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+    for item in sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['count']} times")
+    print("--- End report ---")
 
 
 main()
