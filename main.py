bae930960db7a3af76215a028e46f9c352673b7a
santokan <houska.peter@gmail.com>
2024-10-16 21:40:55 +0200

    added character counter

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_text(book_path)
     words_count = get_words_count(text)
     print(f"Book has {words_count} words.")
+    chars_count = get_char_count(text)
+    print(chars_count)
 
 
 def get_text(path):
@@ -14,6 +16,17 @@ def get_text(path):
 def get_words_count(text):
     words = text.split()
     return len(words)
+
+
+def get_char_count(text):
+    text = text.lower()
+    chars = dict()
+    for char in text:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
 
 
 main()
